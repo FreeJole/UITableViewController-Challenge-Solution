fe6b4ccf0a4b421a12da77e24651d966f7bd505a
Garrett Gosch <garrettgosch@Lindamarias-iMac.local>
2014-09-23 10:38:09 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Garrett Gosch on 9/23/14.
+//  Copyright (c) 2014 MyStuff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Garrett Gosch on 9/23/14.
+//  Copyright (c) 2014 MyStuff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
