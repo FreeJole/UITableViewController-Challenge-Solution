e4e2c73d8d01d5fc4b2e8bd6bc3480167d806492
Garrett Gosch <garrettgosch@Lindamarias-iMac.local>
2014-09-23 11:53:44 -0400

    solved the challenge

--- GGChallengeTableViewController.h
+++ GGChallengeTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  GGChallengeTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Garrett Gosch on 9/23/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface GGChallengeTableViewController : UITableViewController
 
 @end
-
